378148796af66156cfb13db72ff3ccf53c23a1ad
Cicche <140084280+Cicche@users.noreply.github.com>
2026-02-25 11:55:40 +0100

    Fix settings navigation reset, logbook card glitches, and add delay/heading fields

---
+++ SettingsPage.xaml.cs
@@ -0,0 +1,26 @@
+using BaseLogApp.Core.ViewModels;
+
+namespace BaseLogApp.Views;
+
+public partial class SettingsPage : ContentPage
+{
+    private readonly JumpsViewModel _vm;
+
+    public SettingsPage(JumpsViewModel vm)
+    {
+        InitializeComponent();
+        _vm = vm;
+    }
+
+    private async void OnOpenObjectListClicked(object sender, EventArgs e)
+        => await Navigation.PushModalAsync(new NavigationPage(new ObjectListPage(_vm)));
+
+    private async void OnOpenRigListClicked(object sender, EventArgs e)
+        => await Navigation.PushModalAsync(new NavigationPage(new RigListPage(_vm)));
+
+    private async void OnOpenJumpTypeListClicked(object sender, EventArgs e)
+        => await Navigation.PushModalAsync(new NavigationPage(new JumpTypeListPage(_vm)));
+
+    private async void OnOpenDbToolsClicked(object sender, EventArgs e)
+        => await Navigation.PushModalAsync(new NavigationPage(new DbToolsPage(_vm)));
+}
